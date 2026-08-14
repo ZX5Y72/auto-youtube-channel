780df895009bb14e44d03a2a2381a60b00b17741
ZX5Y72 <ziosgaming7@gmail.com>
2026-08-14 08:39:25 +0300

    generate_captions.py

--- generate_captions.py
+++ generate_captions.py
@@ -23,7 +23,7 @@ PlayResY: 1920
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,Arial Black,90,{chosen_color},{chosen_color},&H00000000,&H00000000,1,0,0,0,100,100,0,0,1,4,0,5,10,10,10,1
+Style: Default,{chosen_font},90,{chosen_color},{chosen_color},&H00000000,&H00000000,1,0,0,0,100,100,0,0,1,4,0,5,10,10,10,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
